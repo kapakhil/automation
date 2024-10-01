ed833f084da0831a4b01f338755fc8d6f528f3a3
kapakhil <kapakhil17@gmail.com>
2024-10-02 01:11:58 +0530

    modified

--- code2.sh
+++ code2.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
 ID=$(id -u)
-VALIDATE() {
-if [ $1 -ne 0 ]
+RED='\033[0;31m'
+Green='\033[0;32m' 
+
+if [ $ID - ne 0 ]
 then
-    echo "ERROR : $2.....failed"
+    echo " $RED ERROR : run with root user "
 else
-    echo "$2.......success"
-fi
-}
-
-if [ $ID -ne 0 ]
-then 
-    echo "ERROR ::- you can as a root user"
-else
-    echo "you r the root user u can run"
-fi
-
-yum install mariadb -y
-
-VALIDATE $? "Installing mariadb"
-
-yum install git -y
-
-VALIDATE $? "installing Git"
+    echo " $GREEN installing packages "
+sudo yum install git -y
