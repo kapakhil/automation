d9c0c735d43e94ad9f24faea725c2ddeddb06879
kapakhil <kapakhil17@gmail.com>
2024-10-02 00:28:54 +0530

    second commit

---
+++ code2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ID=$(id -u)
+VALIDATE() {
+if [ $1 -ne 0 ]
+then
+    echo "ERROR : $2.....failed"
+else
+    echo "$2.......success"
+fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "ERROR ::- you can as a root user"
+else
+    echo "you r the root user u can run"
+fi
+
+yum install mariadb -y
+
+VALIDATE $? "Installing mariadb"
+
+yum install git -y
+
+VALIDATE $? "installing Git"
