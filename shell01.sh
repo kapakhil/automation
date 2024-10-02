ff7d7a72c3ec81ac566bed03487cda4c63c7371e
kapakhil <kapakhil17@gmail.com>
2024-10-02 17:32:14 +0530

    code modified

--- shell01.sh
+++ shell01.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+user_id=$(id -u)
+
 Red='\e[31m'
 Green='\e[32m'
 Yellow='\e[33m'
 NC='\e[0m'
 
-Id=$(id -u)
-
-if [ $Id -ne 0 ]
+if [ $user_id -ne 0 ]
 then
     echo " $Red Eroor :$NC $Yellow Sudo user tho run chey broo $NC "
 else 
     echo " $Green Installing required packages $NC "
 fi 
-
 
 sudo yum install ansible -y
